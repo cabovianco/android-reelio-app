4ce36f7b4e783d07fa1e7de72053489de88ac390
thomas <github@cabovianco.anonaddy.me>
2026-03-14 11:08:26 -0300

    Implement DataStoreModule

---
+++ app/src/main/java/com/cabovianco/reelio/di/DataStoreModule.kt
@@ -0,0 +1,24 @@
+package com.cabovianco.reelio.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.dataStoreFile
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataStoreModule {
+    @Provides
+    @Singleton
+    fun providePreferencesDataStore(@ApplicationContext context: Context): DataStore<Preferences> =
+        PreferenceDataStoreFactory.create(
+            produceFile = { context.dataStoreFile("preferences.preferences_pb") }
+        )
+}
